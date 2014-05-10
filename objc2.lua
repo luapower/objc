01d6be2b312f0cc7772df99c866f632d7930c7fc
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-05-10 21:24:43 +0300

    unimportant

--- objc2.lua
+++ objc2.lua
@@ -6,7 +6,7 @@ local ffi = require'ffi'
 local bit = require'bit'
 local expat = require'expat'
 
-assert(ffi.os == 'OSX', 'platform not OSX')
+--assert(ffi.os == 'OSX', 'platform not OSX')
 
 if ffi.abi'64bit' then
 	ffi.cdef[[
@@ -89,19 +89,24 @@ ptrdiff_t ivar_getOffset(Ivar ivar);
 
 ]]
 
+local C = ffi.C
+local objc = {C = C}
+
+objc.debug = false --set to 'cdef' for printing out the cdefs too
+
 --helpers
 
-local function ptr(p)
+local function ptr(p) --convert NULL pointer to nil for easier handling (say `not ptr` instead of `ptr == nil`)
 	if p == nil then return nil end
 	return p
 end
 
-local function nptr(p)
+local function nptr(p) --convert pointer to lua number for using as table key
 	if p == nil then return nil end
 	return tonumber(ffi.cast('intptr_t', p))
 end
 
-local function memoize(func)
+local function memoize(func) --memoize function that can memoize pointer arguments too
 	local cache = {}
 	return function(input)
 		local key = input
@@ -117,16 +122,221 @@ local function memoize(func)
 	end
 end
 
-local function log(topic, fmt, ...)
-	print(string.format('[objc] %-20s %s', topic, string.format(fmt, ...)))
+local function log(topic, fmt, ...) --to be called only when objc.debug is set
+	io.stderr:write(string.format('[objc] %-20s %s\n', topic, string.format(fmt, ...)))
 end
 
-local C = ffi.C
-local objc = {C = C}
+local function cdef(s) --load a cdef via ffi (set objc.debug = 'cdef' to also print those out)
+	local ok, err = pcall(ffi.cdef, s)
+	if not objc.debug then return end
+	if ok then
+		if objc.debug == 'cdef' then
+			print(s)
+		end
+	elseif not err:match'attempt to redefine' then
+		log('cdef-error', '%s\n\t%s', err, s)
+	end
+end
 
---debugging
+local function canread(path) --check that a file is readable without having to open it
+	return C.access(path, bit.lshift(1,2)) == 0
+end
 
-objc.debug = false
+--type parsing/conversion to C types
+
+local type_ctype --fw. decl.
+
+local function optname(name) --format an optional name: if given, return it with a space in front
+	return name and ' '..name or ''
+end
+
+local function array_ctype(s, name) --`[ntype]` -> `type name[n]`
+	local n,s = s:match'^.(%d+)(.-)%]$'
+	return string.format('%s%s[%d]', type_ctype(s, nil, 'sref'), optname(name), n)
+end
+
+--`(uname=type)` -> `uname` or `union uname` depending on deftype
+--`{sname=type}` -> `sname` or `struct sname` depending on deftype
+--`{_NSRect="origin"{_NSPoint=...}...}` -> `struct _NSRect {struct _NSPoint origin; ...};`
+local function struct_ctype(s, name, deftype)
+
+	print(s)
+
+	--break the struct def. in its constituent parts
+	local kw, sname, fields = s:match'^(.)([^%=]+)%=(.-).$' -- `{name=type}`
+	if sname == '?' then sname = nil end -- ? means anonymous
+	if fields == '' then fields = nil end -- empty fields means opaque
+	if not kw then -- no equal sign means opaque, eg. `{OpaqueAEDataStorageType}`
+		kw, sname = s:match'^(.)(.-).$'
+	end
+	kw = kw == '{' and 'struct' or 'union' -- structs are `{...}`, unions are `(...)`
+
+	if not deftype then --don't want the def., only the name
+		return sname
+	end
+
+	if not fields or deftype == 'sref-nodef' then --opaque or wanted as such (can't be anonymous)
+		return string.format('%s %s%s', kw, sname, optname(name))
+	end
+
+	--accumulate field type-name pairs
+	local t = {}
+	local function addfield(name, stype)
+		table.insert(t, type_ctype(stype, name, 'sref')) --results in `struct _NSPoint origin`
+		return '' --remove the match
+	end
+	local s = fields
+	local n
+	while s ~= '' do
+		s,n = s:gsub('^"([^"]+)"(%b{})', addfield) --try "field"{...}
+		if n == 0 then
+			s,n = s:gsub('^"([^"]+)"(%b())', addfield) --try "field"(...)
+		end
+		if n == 0 then
+			s,n = s:gsub('^"([^"]+)"(%b[])', addfield) --try "field"[...]
+		end
+		if n == 0 then
+			s,n = s:gsub('^"([^"]+)"([^"]+)', addfield) --try "field"...
+		end
+		assert(n > 0, s)
+	end
+	local ctype = string.format('%s%s {\n\t%s;\n}', kw, optname(sname), table.concat(t, ';\n\t'))
+
+	if not sname then --anonymous, return its ctype as is
+		return ctype
+	end
+
+	--named struct, cdef it and return its struct-namespace name instead
+	cdef(ctype .. ';')
+	return string.format('%s %s%s', kw, sname, optname(name))
+end
+
+local function bitfield_ctype(s, name) --`bN` -> 'unsigned int name: N`
+	local n = s:match'^b(%d+)$'
+	return string.format('unsigned int %s: %d', name, n)
+end
+
+local function pointer_ctype(s, name, deftype) --`^type` -> `type* name`
+	return string.format('%s*%s', type_ctype(s:sub(2), nil, deftype), optname(name))
+end
+
+local ctype_decoders = {
+	['c'] = 'char',
+	['i'] = 'int',
+	['s'] = 'short',
+	['l'] = 'long', --treated as a 32-bit quantity on 64-bit programs
+	['q'] = 'long long',
+
+	['C'] = 'unsigned char',
+	['I'] = 'unsigned int',
+	['S'] = 'unsigned short',
+	['L'] = 'unsigned long',
+	['Q'] = 'unsigned long long',
+
+	['f'] = 'float',
+	['d'] = 'double',
+
+	['B'] = 'BOOL',
+	['v'] = 'void',
+	['*'] = 'char *',
+
+	['@'] = 'id',
+	['#'] = 'Class',
+	[':'] = 'SEL',
+
+	['['] = array_ctype,    --[Ntype]       ; N = number of elements
+	['{'] = struct_ctype,   --{name=type}   ; struct
+	['('] = struct_ctype,   --(name=type)   ; union
+	['b'] = bitfield_ctype, --bN            ; N = number of bits
+	['^'] = pointer_ctype,  --^type         ; pointer
+	['?'] = 'void',         --unknown type  ; used for function pointers among other things
+}
+
+--convert an objective-c type to a C type.
+--deftype specifies how structs should be parsed and returned:
+--  'sref' means cdef the structs and then reference them as 'struct <sname>' (for bridgesupport struct definitions)
+--  'sref-nodef' is like 'sref' but doesn't cdef the structs (for bridgesupport constants)
+--  'tref' means reference structs as '<name>' and don't cdef their definition (when parsing method types)
+function type_ctype(s, name, deftype)
+	local decoder = ctype_decoders[s:sub(1,1)]
+	assert(decoder, s)
+	if type(decoder) == 'string' then
+		return decoder .. optname(name)
+	end
+	return decoder(s, name, deftype)
+end
+
+local method_ctype = memoize(function(s)
+	local ret_ctype
+	local arg_ctypes = {}
+	for s in s:gmatch'[rnNoORV]?(.-)%d+' do --result-type offset arg1-type offset arg2-type offset ...
+		local ctype = type_ctype(s, nil, 'tref')
+		if not ret_ctype then
+			ret_ctype = ctype
+		else
+			table.insert(arg_ctypes, ctype)
+		end
+	end
+	local meth_ctype = string.format('%s (*) (%s)', ret_ctype, table.concat(arg_ctypes, ', '))
+	if objc.debug then
+		log('method_ctype', '%-20s %s', s, meth_ctype)
+	end
+	return ffi.typeof(meth_ctype)
+end)
+
+--bridgesupport file parsing
+
+local xml = {} --{expat_callback = handler}
+local tag = {} --{tag = start_tag_handler}
+
+local load_xml --fw. decl
+
+function tag.depends_on(attrs)
+	objc.load(attrs.path)
+end
+
+function tag.string_constant(attrs)
+	objc[attrs.name] = attrs.value
+end
+
+function tag.enum(attrs)
+	objc[attrs.name] = tonumber(attrs.value)
+end
+
+local typekey = ffi.abi'64bit' and 'type64' or 'type'
+
+local function cdef_node(attrs, typedecl, deftype)
+	local stype = attrs[typekey] or attrs.type
+	if not stype then return end --type not available on this platform
+	local ctype = type_ctype(stype, attrs.name, deftype)
+	cdef(string.format('%s %s;', typedecl, ctype))
+end
+
+function tag.constant(attrs)
+	cdef_node(attrs, 'const', 'sref-nodef')
+end
+
+function tag.struct(attrs)
+	cdef_node(attrs, 'typedef', 'sref')
+end
+
+function tag.cftype(attrs)
+	cdef_node(attrs, 'typedef', 'sref')
+end
+
+function tag.opaque(attrs)
+	cdef_node(attrs, 'typedef', 'sref-nodef')
+end
+
+function xml.start_tag(name, attrs)
+	if tag[name] then
+		tag[name](attrs)
+	end
+end
+
+function load_xml(path)
+	expat.parse({path = path}, xml)
+end
 
 --loading frameworks
 
@@ -136,22 +346,31 @@ objc.searchpaths = {
 	'~/Library/Frameworks/%s.framework/%s',
 }
 
-function objc.load(name)
-	local canread = bit.lshift(1,2)
+function objc.load(name, bridgesupport)
+	if ffi.os ~= 'OSX' then return end
 	for i,fmt in pairs(objc.searchpaths) do
-		local path = string.format(fmt, name, name)
-		if C.access(path, canread) == 0 then
-			return ffi.load(path, true)
+		local path = fmt:format(name, name)
+		if canread(path) then
+			if bridgesupport ~= false then
+				--bridgesupport files contain typedefs and constants which we can't get from the runtime
+				local path = fmt:format(name, 'Resources/BridgeSupport/'..name..'.bridgesupport')
+				if canread(path) then
+					load_xml(path)
+				end
+				--bridgesupport dylibs contain callable versions of inline functions (NSMakePoint, etc.)
+				local path = fmt:format(name, 'Resources/BridgeSupport/'..name..'.dylib')
+				if canread(path) then
+					ffi.load(path, true)
+				end
+			end
+			ffi.load(path, true)
+			if objc.debug then
+				log('load', path)
+			end
+			return
 		end
 	end
 	error(string.format('[objc] %s not found', name))
-end
-
-if ffi.arch ~= 'arm' then
-	ffi.load('libobjc.A.dylib', true)
-	objc.load'CoreFoundation'
-	objc.load'Foundation'
-	objc.load'Carbon'
 end
 
 --selectors
@@ -184,198 +403,6 @@ local function class(cls)
 	if type(cls) ~= 'string' then return cls end
 	return class_object(cls)
 end
-
---type parsing
-
-local type_ctype --fw. decl.
-
-local function array_ctype(s) --[ntype] (number of elements followed by element type)
-	local n,s = s:match'.^(%d+)(.-)%]$'
-	return string.format('%s[%d]', type_ctype(s), n)
-end
-
-local function union_struct_ctype(s, kw)
-	local name = s:match'^.([^%=]+)'
-	return string.format('%s %s', kw, name)
-end
-
-local function union_ctype(s) --(name=type)
-	return union_struct_ctype(s, 'union')
-end
-
-local function struct_ctype(s) --{name=type}
-	return union_struct_ctype(s, 'struct')
-end
-
-local function bitfield_ctype(s)
-	error'NYI'
-end
-
-local function pointer_ctype(s)
-	return type_ctype(s:sub(2)) .. ' *'
-end
-
-local ctype_decoders = {
-	['c'] = 'char',
-	['i'] = 'int',
-	['s'] = 'short',
-	['l'] = 'long', --treated as a 32-bit quantity on 64-bit programs
-	['q'] = 'long long',
-
-	['C'] = 'unsigned char',
-	['I'] = 'unsigned int',
-	['S'] = 'unsigned short',
-	['L'] = 'unsigned long',
-	['Q'] = 'unsigned long long',
-
-	['f'] = 'float',
-	['d'] = 'double',
-
-	['B'] = 'BOOL',
-	['v'] = 'void',
-	['*'] = 'char *',
-
-	['@'] = 'id',
-	['#'] = 'Class',
-	[':'] = 'SEL',
-
-	['['] = array_ctype,    --[ntype] (number of elements followed by element type)
-	['{'] = struct_ctype,   --{name=type}
-	['('] = union_ctype,    --(name=type)
-	['b'] = bitfield_ctype, --bnum (number of bits)
-	['^'] = pointer_ctype,  --^type
-	['?'] = 'void *',       --unknow type (function pointer, etc.)
-}
-
-function type_ctype(s)
-	local decoder = ctype_decoders[s:sub(1, 1)]
-	assert(decoder, s)
-	return type(decoder) == 'string' and decoder or decoder(s)
-end
-
-local method_ctype = memoize(function(s)
-
-	local ret_ctype
-	local arg_ctypes = {}
-	for s in s:gmatch'[rnNoORV]?(.-)%d+' do --result-type offset arg1-type offset arg2-type offset ...
-		local ctype = type_ctype(s)
-		if not ret_ctype then
-			ret_ctype = ctype
-		else
-			table.insert(arg_ctypes, ctype)
-		end
-	end
-	local meth_ctype = string.format('%s (*) (%s)', ret_ctype, table.concat(arg_ctypes, ', '))
-
-	if objc.debug then
-		log('method_ctype', '%-20s %s', s, meth_ctype)
-	end
-
-	return ffi.typeof(meth_ctype)
-end)
-
---BridgeSupport
-
---[[
-local typekey = ffi.abi'64bit' and 'type64' or 'type'
-
-local _curObj = nil
-local _parseCallbacks = {
-	start_tag = function(name, attrs)
-		-- Methods and classes don't really need to be loaded (keeping these here in case I find otherwise)
-		--elseif name == "class" then
-		--elseif name == "method" then
-		--elseif name == "arg" then
-		--elseif name == "retval" then
-		--elseif name == "signatures" then
-		if name == "string_constant" then
-			rawset(bs, attrs.name, attrs.value)
-			if _loadingGlobally == true then _G[attrs.name] = rawget(bs, attrs.name) end
-		elseif name == "enum" then
-			rawset(bs, attrs.name, tonumber(attrs.value))
-			if _loadingGlobally == true then _G[attrs.name] = rawget(bs, attrs.name) end
-		elseif name == "struct" then
-			if name_clashes[attrs.name] then return end
-			local type = objc.parseTypeEncoding(attrs[typeKey] or attrs.type)
-			if type ~= nil and #type > 0 then
-				type = objc.typeToCType(type[1], nil, true)
-				if type ~= nil then
-					--print("typedef "..type.." "..attrs.name)
-					local success, err = pcall(ffi.cdef, "typedef "..type.." "..attrs.name)
-					if success == false then
-						print("[bs] Error loading struct "..attrs.name..": "..err)
-					else
-						rawset(bs, attrs.name, ffi.typeof(attrs.name))
-						if _loadingGlobally == true then _G[attrs.name] = rawget(bs, attrs.name) end
-					end
-				end
-			end
-		--elseif name == "field" then
-		elseif name == "cftype" then
-			local type = objc.parseTypeEncoding(attrs[typeKey] or attrs.type)
-			if type ~= nil and #type > 0 then
-				type = objc.typeToCType(type[1], attrs.name)
-				if type ~= nil then
-					--_log("typedef "..type)
-					ffi.cdef("typedef "..type)
-				end
-			end
-		elseif name == "constant" then
-			local type = objc.parseTypeEncoding(attrs[typeKey] or attrs.type)
-			if type ~= nil and #type > 0 then
-				type = objc.typeToCType(type[1], attrs.name)
-				if type ~= nil then
-					--_log(type)
-					ffi.cdef(type)
-				end
-			end
-		elseif name == "function" then
-			_curObj = {}
-			_curObj.type = "function"
-			_curObj.name = attrs.name
-			_curObj.args = {}
-			_curObj.retval = "v"
-		elseif _curObj ~= nil and name == "arg" then
-			local type = attrs[typeKey]  or attrs.type
-			if type == "@?" then type = "@" end -- Apple seems to have gone crazy and added a weird special case for block definitions
-			table.insert(_curObj.args, type)
-		elseif _curObj ~= nil and name == "retval" then
-			_curObj.retval = attrs[typeKey] or attrs.type
-		elseif name == "depends_on" then
-			-- If dependency loading is off we still load nested frameworks
-			local n = 0
-			for o in attrs.path:gfind(".framework") do n=n+1 end
-			if bs.loadDependencies == true or n >= 2 then
-				bs.loadFramework(attrs.path, _loadingGlobally, true)
-			end
-		--elseif name == "opaque" then
-		--elseif name == "informal_protocol" then
-		--elseif name == "function_alias" then
-		end
-	end,
-	end_tag = function(name)
-		if name == "function" then
-			local sig       = _curObj.retval..table.concat(_curObj.args)
-			local signature = objc.impSignatureForTypeEncoding(sig, _curObj.name)
-			local name = _curObj.name
-			--print(signature)
-			if signature ~= nil then
-					bs[name] = function(...)
-						bs[name] = ffi.cdef(signature)
-					if _loadingGlobally == true then
-						_G[name] = C[name]
-					end
-					return bs[name](...)
-				end
-				if _loadingGlobally == true then
-					_G[name] = bs[name]
-				end
-			end
-			_curObj = nil
-		end
-	end
-}
-]]
 
 --protocols
 
@@ -458,7 +485,7 @@ local function create_method_wrapper(get_method, class, sel_str)
 		local ok, ret = pcall(imp, ffi.cast(id_ctype, self), sel, ...)
 
 		if not ok then
-			error(string.format('%s\n%s', ret, debug.traceback()))
+			error(string.format('[objc] %s\n%s', ret, debug.traceback()))
 		end
 
 		if ret ~= nil and ffi.istype(id_ctype, ret) then
@@ -609,11 +636,30 @@ function objc.class(classname, superclass)
 	return class
 end
 
+--load the runtime and base frameworks
+
+if ffi.arch ~= 'arm' and ffi.os == 'OSX' then
+	ffi.load('libobjc.A.dylib', true)
+	objc.load'CoreFoundation'
+	objc.load'Foundation'
+	objc.load'Carbon'
+end
+
 
 if not ... then
 
-objc.debug = true
+objc.debug = 'cdef'
+local unit = require'unit'
+for i,f in ipairs(dir'2') do
+	if f == 'CoreFoundation.bridgesupport' then
+		print(('='):rep(80))
+		print(f)
+		print(('='):rep(80))
+		load_xml('2/'..f)
+	end
+end
 
+--[[
 objc.load'Foundation'
 objc.load'AppKit'
 objc.load'System'
@@ -621,6 +667,7 @@ objc.load'CoreServices'
 
 local NSWin = objc.class('NSWin', 'NSWindow')
 objc.conforms('NSWin', 'NSWindowDelegate')
+]]
 
 --local m = objc.NSWin:alloc()
 --print(m)
